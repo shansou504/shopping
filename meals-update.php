91481f94d04f47519793dbd160080d6d9098d994
shansou504 <123512155+shansou504@users.noreply.github.com>
2023-09-07 14:32:41 -0400

    removed files from old version

--- meals-update.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('connection.php');
-$id = $_POST['id'];
-$mealstext = $_POST['mealstext'];
-$sqlmealsupdate = mysqli_query($mysqli, "UPDATE meals SET mealstext = '$mealstext' WHERE id = '$id'");
-header('Location: index.php');
-?>
